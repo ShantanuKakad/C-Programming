4bc295f43a19a5491a055bdfb9237578e6017362
Shantanu Kakad <163027166+ShantanuKakad@users.noreply.github.com>
2026-02-02 18:56:17 +0530

    Basic arithmetic operations in operators.c
    Here I learned about different operators in C.  i.e. Addition, Substraction, Multiplication , Division .
    Most importantly I learned about modulo operator that gives us Remainder.

---
+++ 02_Operators/operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 10, b = 3;
+
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Remainder: %d\n", a % b);
+
+    return 0;
+}
